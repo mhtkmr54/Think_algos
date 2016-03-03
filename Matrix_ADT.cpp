58ebf60dc5bdd969ebbf0ed50bf9cdda914813dd
mhtkmr54 <mhtkmr54@users.noreply.github.com>
2016-03-04 01:47:32 +0530

    MATRIX ADT + 6.2

--- Matrix_ADT.cpp
+++ Matrix_ADT.cpp
@@ -26,6 +26,38 @@ for (i=0;i<M1.matDimension;i++)
 for (j=0; j<M1.matDimension;j++){
 resultMatrix.matValues[i][j] = 0;
 }
+
+
+//#include <stdio.h>
+Stack returnStackWithNearestGreater(Stack inputStack)
+{
+  int a,i,k;
+  int arr[100];
+  //printf("stack to be init");
+  Stack toreturn;
+  int neg = -1;
+  toreturn.push(neg);
+  while (inputStack.pop() != -1){
+     i = 0;
+    arr[i] = inputStack.pop();
+    i++;
+  }
+  int length = i;
+  int c = i-2;
+  while (c != 0){
+    for (k = c+1 ;k < length;k++ ){
+      if (arr[c] < arr[k]){
+           toreturn.push(arr[k]);
+           break;
+      }
+      toreturn.push(neg); 
+
+     }
+      c = c -1;
+    }
+   //printf("to be return oh yeahhh");
+   return toreturn;
+}
 for (i=0;i<M1.matDimension;i++)
 for (j=0; j<M1.matDimension;j++)
 for (k=0;k<M1.matDimension;k++){
